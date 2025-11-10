1646f1ff1130608738a35a3e245370a443026c95
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-11-10 09:34:11 +0000

    turn off supervisor.status_bar
    Without this change, when I log USB serial output with tio, I get
    escape sequence garbage characters from the CircuitPython
    supervisor's status bar feature. This commit turns off the status
    bar.

--- boot.py
+++ boot.py
@@ -1,5 +1,6 @@
 import board
 import digitalio
+import supervisor
 import usb_hid
 
 usb_hid.disable()
@@ -7,3 +8,10 @@ usb_hid.disable()
 # Toggle A0 to mark start of boot.py for power profiler
 a0 = digitalio.DigitalInOut(board.A0)
 a0.switch_to_output(value=True)
+
+# Disable status bar to stop the default USB serial escape sequence noise
+try:
+    supervisor.status_bar.console = False
+    supervisor.status_bar.display = False
+except Exception:
+    pass
